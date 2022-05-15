e987bf37602b0086eb4a1005786347b5857420c8
Victor Yuri <victoryuritc@yahoo.com>
2022-05-15 03:35:39 -0300

    Tentativa de resolução do desafio circle com toHaveLength, exclamaçãoradius, typeof circle, typeof radius e toEqual.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,8 +35,8 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     expect(typeof productDetails).toBe('function'); // Teste se productDetails é uma função.
     expect(typeof productDetails()).toBe('array'); // Teste se o retorno da função é um array.
     expect(productDetails()).toHaveLength(2); // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect() // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect() // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     expect() // Teste se os dois productIds terminam com 123.
   });
 });
