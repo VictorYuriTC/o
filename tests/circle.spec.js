afdd06cbf820d030acfed0ffa77c96842e52cfeb
Victor Yuri <victoryuritc@yahoo.com>
2022-05-14 23:32:56 -0300

    Retificação do productDetails, já que eu havia retirado a função fail e, por isso, o desafio estava completo sem eu haver codado algo.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,7 +28,7 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
      // Teste se circle retorna um objeto.
     // Teste se o objeto retornado possui 3 propriedades.
     expect(circle(!radius)).toBeUndefined(); // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
+    expect(circle(2)).toEqual(circumference = 2 * 3.14 * 2); // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
   });
