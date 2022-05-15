628cd1a067cf6ba19f06ab0dde71f6a8f1232d7e
Victor Yuri <victoryuritc@yahoo.com>
2022-05-15 01:57:42 -0300

    Utilização de typeof e toHaveLength para satisfazer 3 quesitos do desafio productDetails.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,11 +31,10 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
-    expect // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
+    expect(typeof productDetails).toBe('function'); // Teste se productDetails é uma função.
+    expect(typeof productDetails()).toBe('array'); // Teste se o retorno da função é um array.
+    expect(productDetails()).toHaveLength(2); // Teste se o array retornado pela função contém dois itens dentro.
     // Teste se os dois itens dentro do array retornado pela função são objetos.
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
