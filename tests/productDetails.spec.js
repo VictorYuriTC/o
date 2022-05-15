18911603a9f8b10bc5cf0377a74b70741b7c4c9d
Victor Yuri <victoryuritc@yahoo.com>
2022-05-15 04:24:38 -0300

    Ajustes no desafio productDetails. Criação de constante para armazenar um return da função.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -32,11 +32,12 @@ const productDetails = require('../src/productDetails');
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
     // ESCREVA SEUS TESTES ABAIXO:
+    const productDetailsReturn = productDetails('Luva látex', 'Bisturi');
     expect(typeof productDetails).toBe('function'); // Teste se productDetails é uma função.
     expect(typeof productDetails()).toBe('array'); // Teste se o retorno da função é um array.
-    expect(productDetails()).toHaveLength(2); // Teste se o array retornado pela função contém dois itens dentro.
-    expect() // Teste se os dois itens dentro do array retornado pela função são objetos.
-    expect() // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    expect() // Teste se os dois productIds terminam com 123.
+    expect(productDetailsReturn).toHaveLength(2); // Teste se o array retornado pela função contém dois itens dentro.
+    // Teste se os dois itens dentro do array retornado pela função são objetos.
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    // Teste se os dois productIds terminam com 123.
   });
 });
